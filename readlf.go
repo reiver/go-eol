d3a61fafefcae76f09188942bfb82e190a6b958e
Charles Iliya Krempeaux <iamreiver@gmail.com>
2023-11-28 08:08:06 -0800

    initial commits

--- readlf.go
+++ readlf.go
@@ -2,9 +2,12 @@ package eol
 
 import (
 	"io"
+
+	"sourcecode.social/reiver/go-opt"
 )
 
 func ReadLF(runescanner io.RuneScanner) (size int, err error) {
-	const runeNumber = 1
-	return readthisrune(runescanner, lf, runeNumber)
+	const characterNumber uint64 = 1
+	var circumstance internalCircumstance = specifyCircumstance(opt.Something(LF), characterNumber)
+	return readthisrune(circumstance, runescanner, lf)
 }
