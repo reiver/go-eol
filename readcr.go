81ff90ccd8c1ef8853791919a18f15cdea556f92
Charles Iliya Krempeaux <iamreiver@gmail.com>
2023-11-28 08:07:35 -0800

    initial commits

--- readcr.go
+++ readcr.go
@@ -2,9 +2,12 @@ package eol
 
 import (
 	"io"
+
+	"sourcecode.social/reiver/go-opt"
 )
 
 func ReadCR(runescanner io.RuneScanner) (size int, err error) {
-	const runeNumber = 1
-	return readthisrune(runescanner, cr, runeNumber)
+	const characterNumber uint64 = 1
+	var circumstance internalCircumstance = specifyCircumstance(opt.Something(CR), characterNumber)
+	return readthisrune(circumstance, runescanner, cr)
 }
